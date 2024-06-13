c99692a386e7618a53d1d5a49b1eb12e6baac6cf
Luciano Alfredo <lalfredo@c2r10s1.42luanda.com>
2024-06-13 17:00:50 +0100

    C01

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lalfredo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 09:06:06 by lalfredo          #+#    #+#             */
+/*   Updated: 2024/06/13 15:10:48 by lalfredo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	i = 0;
+	while (i < (size / 2))
+	{
+		swap = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = swap;
+		i++;
+	}
+}
